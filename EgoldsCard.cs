b245b0ac066f3da4460674c505c0de2355fb8ba1
Владимир лазарев <vova.lazarev199@gmail.com>
2022-10-21 19:49:22 +0500

    надо

---
+++ EgoldsCard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Is_1_20_LazarevVA
+{
+    public class EgoldsCard: Control
+    {
+        #region Переменные
+        private float CurtainHeight;//высота шторки баз
+
+
+        #endregion
+        public EgoldsCard()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.SupportsTransparentBackColor | ControlStyles.UserPaint, true);
+            DoubleBuffered = true;
+
+            Size = new Size(250, 200);
+            CurtainHeight = Height - 60;
+
+            Font = new Font("Verdana", 9F, FontStyle.Regular);
+            BackColor = Color.White;
+            CurtainHeight = Height - 60;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics graph = e.Graphics;
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+
+            graph.Clear(Parent.BackColor);
+
+            Rectangle rect = new Rectangle(0, 0, Width - 1, Height - 1);
+            Rectangle rectCurtain = new Rectangle(0, 0, Width - 1, (int)CurtainHeight);
+
+        }
+    }
+}
